7b988d9ef69f3d60f37b91faec4204a4859963f8
jennprk <jieunprk@gmail.com>
2022-04-17 23:06:13 -0400

    Initial commit

---
+++ R/diffsig-package.R
@@ -0,0 +1,16 @@
+#' The 'diffsig' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name diffsig-package
+#' @aliases diffsig
+#' @useDynLib diffsig, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2021). RStan: the R interface to Stan. R package version 2.21.3. https://mc-stan.org
+#'
+NULL
