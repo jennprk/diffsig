b4bb9a0cc25bbc75ad3f66158904accdda1f519b
jennprk <jieunprk@gmail.com>
2022-04-28 02:15:16 -0400

    Update diffsig_plot.R

--- R/diffsig_plot.R
+++ R/diffsig_plot.R
@@ -7,9 +7,11 @@
 #' @param ... Arguments passed to ggplot
 #' @return Diffsig plot
 #'
-diffsig_plot <- function(fit, pars="beta", rowlabels=NULL, ...) {
+diffsig_plot <- function(fit, pars="beta", rowlabels=NULL, font=NULL) {
   require(rstan)
-  p <- stan_plot(fit=fit,pars=pars)
+  require(ggplotify)
+
+  p <- stan_plot(fit=fit,pars=pars, fill_color="#0C7BDC", outline_color="black", est_color="#FFC20A")
   p <- p  +
     scale_x_continuous(breaks=c(layer_scales(p)$x$range$range[1], 0, layer_scales(p)$x$range$range[2]),
                        labels = c("Less Common","0","More Common")) +
@@ -23,12 +25,15 @@ diffsig_plot <- function(fit, pars="beta", rowlabels=NULL, ...) {
                                r = 30,  # Right margin
                                b = 5,  # Bottom margin
                                l = 5)) +
-    labs(x = NULL)
+    labs(x = NULL)  +
+    theme(text=element_text(size=16,  family=font, face = "plain"),
+          axis.text.y = element_text(size=15,  family=font, face = "bold"))
 
+  pp = ggplot_build(p)
   ## re-label y-axis with names
   pp$layout$panel_params[[1]]$y.sec$scale$labels <- rowlabels
   ppp <- ggplot_gtable(pp)
-  p_fin <- plot(ppp) + ...
+  p_fin <- as.ggplot(ppp)
 
   return(p_fin)
 }
