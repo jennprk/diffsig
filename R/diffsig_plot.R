5848934d4a764f81699c59768ad5998ce635edde
jennprk <jieunprk@gmail.com>
2022-05-17 16:02:34 -0400

    Update diffsig_plot.R

--- R/diffsig_plot.R
+++ R/diffsig_plot.R
@@ -26,7 +26,8 @@ diffsig_plot <- function(fit, pars, rowlabels, rowgroup=NULL, est_color=NULL, co
   p.base <- ggplot2::ggplot(xy.df)
   p.name <- ggplot2::scale_y_continuous(breaks = y, labels = rowlabels,
                                         limits = c(0.8, max(y) + 0.2))
-  p.all <- p.base + ggplot2::xlim(xlim.use) + p.name + theme_bw()
+  p.all <- p.base + ggplot2::xlim(xlim.use) + p.name + geom_vline(xintercept=0, linetype="dashed",color="darkgrey") +
+    theme_ipsum()
 
   p.ci <- ggplot2::geom_segment(mapping = ggplot2::aes_string(x = "ll", xend = "hh", y = "y", yend = "y"))
   p.all <- p.all + p.ci
@@ -45,20 +46,23 @@ diffsig_plot <- function(fit, pars, rowlabels, rowgroup=NULL, est_color=NULL, co
     }
 
     p.ci.2 <- ggplot2::geom_segment(ggplot2::aes_string(x = "l", xend = "h", y = "y", yend = "y"), color = color_by, size = 2)
-    p.point <- ggplot2::geom_point(ggplot2::aes_string(x = "m", y = "y"), shape = 19, color=est_color, size = 3)
+    p.point <- ggplot2::geom_point(ggplot2::aes_string(x = "m", y = "y"), shape = 19, color=color_by, size = 2, show.legend = F)
+    p.point2 <- ggplot2::geom_point(ggplot2::aes_string(x = "m", y = "y"), shape = 19, color=est_color, size = 1.5)
+
     p.all <- p.all + p.ci.2 +
-      xlab("beta") +
+      xlab(expression(beta)) +
       theme(axis.title.y=element_blank())
   } else {
     color_by <- "statmat$group"
 
     p.ci.2 <- ggplot2::geom_segment(ggplot2::aes_string(x = "l", xend = "h", y = "y", yend = "y", color = color_by), size = 2)
-    p.point <- ggplot2::geom_point(ggplot2::aes_string(x = "m", y = "y"), shape = 19, color=est_color, size = 3)
+    p.point <- ggplot2::geom_point(ggplot2::aes_string(x = "m", y = "y", color=color_by), shape = 19, size = 2, show.legend = F)
+    p.point2 <- ggplot2::geom_point(ggplot2::aes_string(x = "m", y = "y"), color = est_color, shape = 19, size = 1.5)
 
     if(is.null(colors)) {
       p.all <- p.all +
         p.ci.2 +
-        scale_color_viridis(discrete = TRUE, begin = 0.7, end=0.4, option="D", name="Risk Factors") +
+        scale_color_viridis(discrete = TRUE, begin = 0.8, end=0.2, option="A", name="Risk Factors") +
         xlab(expression(beta)) +
         theme(axis.title.y=element_blank())
     } else {
@@ -69,40 +73,37 @@ diffsig_plot <- function(fit, pars, rowlabels, rowgroup=NULL, est_color=NULL, co
       p.all <- p.all +
         p.ci.2 +
         scale_color_manual(name="Risk Factors",values=colors) +
-        xlab("beta") +
+        xlab(expression(beta)) +
         theme(axis.title.y=element_blank())
     }
   }
 
-  p <- p.all + geom_vline(xintercept=0, linetype="dashed",color="darkgrey") + p.point
+ p <- p.all + p.point +  p.point2
 
-  ggbld <- ggplot_build(p)
-  p <- p +
+ (p <- p +
     theme(legend.position="bottom",
-          # legend.direction="horizontal",
-          # legend.background = element_rect(size=0.5, linetype="solid", colour ="black"),
-          legend.text = element_text(size=10, face = "plain"),
-          # panel.grid.major.x = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          axis.line = element_line(colour = "black"),
-          # legend.title = element_blank(),
-          axis.title.y=element_blank()) +
+          panel.grid.minor.y = element_blank(),
+          panel.grid.minor.x = element_line(size = 0.05),
+          axis.title.y = element_blank()) +
     # scale_x_continuous(breaks=ggbld$layout$panel_params[[1]]$x.sec$breaks,
                        # labels=c("less common",ggbld$layout$panel_params[[1]]$x.sec$breaks[-c(1, length(ggbld$layout$panel_params[[1]]$x.sec$breaks))], "more common")) +
    # scale_x_continuous(breaks = c(layer_scales(p)$x$range$range[1],ggbld$layout$panel_params[[1]]$x.sec$breaks,layer_scales(p)$x$range$range[2]),
                        # labels = c("Less Common",ggbld$layout$panel_params[[1]]$x.sec$breaks,"More Common")) +
     theme(axis.ticks.x = element_blank(),
           axis.line.x = element_line(arrow = grid::arrow(length = unit(0.3, "cm"),
-                                                         ends = "both")),
-          axis.title.x = element_text(angle = 0),
+                                                         ends = "both"), color="grey27"),
+          axis.title.x = element_text(angle = 0, margin=margin(-5,0,0,0)),
           axis.text.x = element_text(size="10"),
           panel.grid.major.x = element_line(),
           plot.margin = margin(t = 5,  # Top margin
                                r = 30,  # Right margin
                                b = 5,  # Bottom margin
-                               l = 5))
-  p
+                               l = 5),
+          legend.margin = margin(t=-15),
+          legend.spacing.x = unit(0.1, 'cm')) +
+      guides(colour = guide_legend(ncol = 3))
+    )
+
   # pp = ggplot_build(p)
   # ## re-label y-axis with names
   # pp$layout$panel_params[[1]]$y.sec$scale$labels <- rowlabels
